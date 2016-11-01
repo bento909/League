3111da120da8cf346695627d4e3817cbf74d3840
Bento909 <benjaminthomas808@gmail.com>
2016-11-01 20:50:09 +0000

    Delete this branch. It is ruined

--- src/main/java/com/bento909/model/League.java
+++ src/main/java/com/bento909/model/League.java
@@ -1,13 +1,23 @@
 package com.bento909.model;
 
-import java.util.Set;
-import java.util.TreeSet;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.*;
 
 public class League {
 
     private final TreeSet<Team> teams;
-
-    public League(Set<Team> teams) {
-        this.teams = new TreeSet<>(teams);
+    //make this set an immutable set
+    private League(Set<Team> teams) {
+        ImmutableSet.Builder<League> league = ImmutableSet.builder();
+        for (Set.Entry<>)
     }
+}
+
+
+//    private final TreeSet<Team> teams;
+
+//    public League(Set<Team> teams) {
+//       this.teams = new TreeSet<>(teams);
+//    }
 }
