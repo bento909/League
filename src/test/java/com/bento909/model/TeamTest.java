6cf192e7e1f5063edd423b08f5088fcb9b2cddc0
Bento909 <benjaminthomas808@gmail.com>
2016-11-07 20:00:30 +0000

    I have made some minor changes to TeamTest but I can't believe they are right, or what you had in mind for me to do.

--- src/test/java/com/bento909/model/TeamTest.java
+++ src/test/java/com/bento909/model/TeamTest.java
@@ -5,8 +5,9 @@ import com.google.common.collect.ImmutableList;
 public class TeamTest {
 
     private static final Team TEST_TEAM = new Team("Test1", ImmutableList.of(
-            Match.builder()
+            Match.builder("Everton", 12/22/2016, "Goodison Park", "Gareth Barry",new Result(0,1) )
                     .build(),
+            //Steve, this can't be right? Surely you have to call withOpposition etc?
             Match.builder()
                     .build()));
 
