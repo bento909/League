69d2444cab40d36247c9c79c07c6f62440fdb97c
Bento909 <benjaminthomas808@gmail.com>
2016-11-18 14:02:52 +0000

    Have made a class which will generate a random date between 2000 and 2016

---
+++ src/main/java/com/bento909/model/DateMaker.java
@@ -0,0 +1,49 @@
+package com.bento909.model;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.text.ParseException;
+
+public class DateMaker {
+    // Generates a date at random
+    public Date dateMaker() {
+        //ArrayList<> month31Days = new List<1,3,5,7,8,10,12>;
+        Date date;
+
+        try {
+            DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+            date = df.parse(dateString());
+            return date;
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    private String dateString(){
+        Random rand = new Random();
+        int year = rand.nextInt(16) + 2001;
+        int month = rand.nextInt(12) + 1;
+        int days = 0;
+        int day;
+        String theDate;
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            days = 30;
+            if (month == 2) {
+                if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0)) {
+                    days = 29;
+                } else {
+                    days = 28;
+                }
+            } else {
+                days = 31;
+            }
+        }
+        day = rand.nextInt(days) + 1;
+        theDate = String.valueOf(day).concat("-").concat(String.valueOf(month))
+                .concat("-").concat(String.valueOf(year));
+        return theDate;
+    }
+}
+
--- src/test/java/com/bento909/model/TeamTest.java
+++ src/test/java/com/bento909/model/TeamTest.java
@@ -2,16 +2,20 @@ package com.bento909.model;
 
 import com.google.common.collect.ImmutableList;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class TeamTest {
     Calendar calendar = new GregorianCalendar(2013,10,28);
-
+    SimpleDateFormat dF = new SimpleDateFormat("dd/MM/yyyy");
+    try
+        Date date1 = dF.parse("19/09/1980");
+    catch(parseException e)
     private static final Team TEST_TEAM = new Team("Test1", ImmutableList.of(
             Match.builder().withOpposition("Everton"),
-            Match.builder().withMatchPlayed(19/09/1980),
+            Match.builder().withMatchPlayed(dF.parse("19/09/1980")),
             Match.builder().withVenue("Goodison Park"),
             Match.builder().withManOfTheMatch("Gareth Barry"),
             Match.builder().withResult(new Result(0,1))
